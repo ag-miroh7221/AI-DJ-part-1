6e034efee81704539e0219082dd4a85150cdd442
Aanya <85621509+ag-miroh7221@users.noreply.github.com>
2024-05-25 22:04:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+song1 = "";
+song2 = "";
+
+function preload() {
+   song1 = loadSound("music.mp3");
+   song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(650 , 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet Is Intitialized!");
+}
+
+function draw() {
+    image(video, 0, 0, 650, 400);
+}
+
+
 
 
 
