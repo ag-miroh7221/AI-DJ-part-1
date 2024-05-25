60d4f799f3c9303d3d39663ba4db60f9009964e2
Aanya <85621509+ag-miroh7221@users.noreply.github.com>
2024-05-25 20:37:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+//function play() {
+ //   song.play();
+  //  document.getElementById("pause_btn").style.display="inline-block";
+  //  document.getElementById("play_btn").style.display = "none";
+
+ //   song.setVolume(1);
+   // song.rate(1);
+//}   //
+
+//function pause() {
+   // song.pause();
+ //   document.getElementById("play_btn").style.display="inline-block";
+   // document.getElementById("pause_btn").style.display = "none";
+//}   //
+
